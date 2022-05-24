a60fd5fa98eb893213fa9457a9636e5b4a8e5dff
Eight-Corner <corner3499@gmail.com>
2022-05-25 00:28:37 +0900

    헤더 - 로고

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './assets/css/util.css';
 import "antd/dist/antd.min.css";
 import Main from "./pages/main";
 import {
@@ -10,9 +11,14 @@ import {PageHeader} from "antd";
 function App() {
     return (<div class="tft-app-body">
         <div id="header">
-            <img src="./assets/images/logo512.png" id="header-logo" alt=""/>
-            <PageHeader className="site-page-header" onBack={() => null} title="롤토체스 초심자를 위한 가이드"
-                        subTitle="아직 미정입니다."/>
+            <div className="d-flex w-100-p header-box">
+                <img src="./assets/images/logo512.png" id="header-logo" alt=""/>
+                <PageHeader className="site-page-header" onBack={
+                    () => null
+                } title="롤토체스 초심자를 위한 가이드"
+                            subTitle="아직 미정입니다.">
+                </PageHeader>
+            </div>
         </div>
         <div id="body">
             <Routes>
