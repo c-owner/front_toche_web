82eaeacc6a33be6340b7f36c76d69b4fa4a62d4f
Eight-Corner <corner3499@gmail.com>
2022-05-24 22:49:11 +0900

    TFT 초심자가이드 REACT SET UP DONE

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import "antd/dist/antd.min.css";
+
+function App() {
+    return (
+        <div>
+            <div id="header">
+                <div id="header-area">
+                        <img src="./logo.svg" alt=""/>
+                </div>
+            </div>
+            <div id="body">
+            
+            </div>
+            <div id="footer"></div>
+        </div>
+    );
+}
+
+export default App;
