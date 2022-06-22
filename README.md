c23915864097fc35152daf25453b19bd7da84dc6
corner <corner3499@gmail.com>
2022-06-22 22:35:11 +0900

    reset

---
+++ README.md
@@ -0,0 +1,15 @@
+# TFT_초심자_GUIDE WEB SERVER
+
+```
+Frontend Develoepr : corner,
+Backend Developer : heekng, 
+```
+## Usage Skill || Stack
+- React
+  - Ant Design
+
+- Java Spring
+
+
+[Connect ::👉 Frontend Temporary Web Server](https://front-toche-web.vercel.app/)
+
---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+export default {
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'TFT 초심자를 위한 가이드',
+        htmlAttrs: {
+            lang: 'ko'
+        },
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: ''},
+            {name: 'format-detection', content: 'telephone=no'}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/logo192.png'}
+        ]
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        'element-ui/lib/theme-chalk/index.css',
+        '~assets/scss/index.scss'
+    ],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        '@/plugins/element-ui'
+    ],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        transpile: [/^element-ui/],
+    },
+
+//   server port
+    server: {
+        port: 3000,
+    },
+}
