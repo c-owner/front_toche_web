e8cee8448bb0042e4085400bc8bd5de819557295
corner <corner3499@gmail.com>
2022-06-28 00:02:51 +0900

    [Frontend] test
    내용 :test
    
    이슈 :

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,94 @@
 <template>
     <div>
-        <div>
+        <el-tabs type="border-card" v-model="tabIndex" @tab-click="tabClick">
+            <el-tab-pane :label="seasonInfo.title" >
 
-        </div>
+            </el-tab-pane>
+            <el-tab-pane label="챔피언 목록">
+                <ul>
+                    <li v-for="(unit, u_idx) in $store.state.unitList" :key="unit.id" class="unit-list">
+                        <div class="unit-box">
+                            <div class="unit-img-box">
+                                <img src="~/assets/images/favicon.png" :src="unit.iconPath" alt="챔피언 이미지">
+                            </div>
+                            <div class="unit_name">{{ unit.krName }}</div>
+                        </div>
+                    </li>
+                </ul>
+            </el-tab-pane>
+            <el-tab-pane label="아이템 목록">
+
+            </el-tab-pane>
+            <el-tab-pane label="추천 덱">
+
+            </el-tab-pane>
+            <el-tab-pane label="증강">
+
+            </el-tab-pane>
+            <el-tab-pane label="특성">
+
+            </el-tab-pane>
+
+        </el-tabs>
+
+
     </div>
 </template>
 
 <script>
 export default {
-    name: 'IndexPage',
+    name: "Index",
+    data() {
+        return {
+            tabIndex: '',
+        }
+    },
+    created() {
+    },
+    mounted() {
+        if (this.seasonInfo) {
+            this.getUnit()
+        }
+    },
+    beforeDestroy() {
+    },
+    computed: {
+        seasonInfo() {
+            return this.$store.getters['seasonInfo'];
+        },
+        units() {
+            return this.$store.getters['units'];
+        }
+    },
+    methods: {
+        tabClick(key) {
+            
+        },
+        getUnit() {
+            let seasonId;
+            if (this.seasonInfo.hasOwnProperty('id')) {
+                seasonId = this.seasonInfo.id;
+            } else {
+                seasonId = 14;
+            }
+            try {
+                let params = {
+                    seasonId: seasonId,
+                };
+                this.$store.dispatch('getUnitList', params).then(res => {
+                    this.$store.commit('setUnitList', res);
+                });
+
+            } catch (e) {
+                console.log(e)
+            }
+        },
+
+    }
+
 }
 </script>
+
+<style scoped>
+
+</style>
