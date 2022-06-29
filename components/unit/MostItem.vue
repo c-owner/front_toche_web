2fe781b6385fa7acdc74a8d2a6e11f9286fe1f73
corner <corner3499@gmail.com>
2022-06-29 22:19:02 +0900

    챔피언 상세 - 최다 아이템 조회 API -

---
+++ components/unit/MostItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="pt16">
+      <div v-for="(item, i_idx) in mostItem" :key="item.id" class="mostitem_list">
+          <div class="mostitem_icon">
+
+          </div>
+          <div class="mostitem_count" style="color: #ff9900">
+            {{ item.championUsedCount }}
+          </div>
+          <div class="mostitem_desc" v-html="item.desc"></div>
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MostItem",
+    props: ['mostItem'],
+}
+</script>
+
+<style scoped>
+
+</style>
