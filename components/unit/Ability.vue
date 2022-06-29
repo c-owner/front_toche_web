a94d686c3d8108800142a918a211afd33e726f01
corner <corner3499@gmail.com>
2022-06-29 13:02:00 +0900

    상세

---
+++ components/unit/Ability.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="unit_ability_info">
+        <img class="unit_img_box" :src="ability.iconPath" width="45" height="45" alt="챔피언 특성 이미지"/>
+        <div class="ability_title">{{ ability.krName }}
+            <div class="ability_desc" v-html="ability.abilityDesc">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Ability",
+    props: ['ability']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/unit/Stats.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="unit_status">
+        <div class="status_box">
+            <div class="status_title">체력</div>
+            <div class="status_value">{{ stats.hp }}</div>
+        </div>
+        <div class="status_box">
+            <div class="status_title">공격력</div>
+            <div class="status_value">{{ stats.damage }}</div>
+        </div>
+        <div class="status_box">
+            <div class="status_title">마나</div>
+            <div class="status_value">{{ stats.initialMana}} / {{ stats.mana }}</div>
+        </div>
+        <div class="status_box">
+            <div class="status_title">DPS</div>
+            <div class="status_value">{{ stats.attackSpeed }}</div>
+        </div>
+        <div class="status_box">
+            <div class="status_title">방어력</div>
+            <div class="status_value">{{ stats.armor }}</div>
+        </div>
+        <div class="status_box">
+            <div class="status_title">마법저항력</div>
+            <div class="status_value">{{ stats.magicResist }}</div>
+        </div>
+        <div class="status_box">
+            <div class="status_title">치명타</div>
+            <div class="status_value">{{ stats.critChance }}</div>
+        </div>
+        <div class="status_box">
+            <div class="status_title">치명타 배율</div>
+            <div class="status_value">{{ stats.critMultiplier }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Stats",
+    props: ['stats']
+}
+</script>
+
+<style scoped>
+
+</style>
