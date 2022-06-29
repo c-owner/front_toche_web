38d6edf3e170d6e62e1cd367a57c289f6344b4d0
corner <corner3499@gmail.com>
2022-06-29 22:56:52 +0900

    챔피언 최다 아이템조회 완료

--- components/unit/MostItem.vue
+++ components/unit/MostItem.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="pt16">
-      <div v-for="(item, i_idx) in mostItem" :key="item.id" class="mostitem_list">
-          <div class="mostitem_icon">
-
+      <div v-for="(item, i_idx) in mostItem" :key="item.id" class="mb30 most_list">
+          <div class="mostitem_icon flex align-center p-relative">
+              <div class="unit_img_box p-relative" :style="`background: url(${item.iconPath}) center / contain`">
+                  <el-badge :value="item.championUsedCount" class="item p-absoulte"></el-badge>
+              </div>
+              <div class="in_block pl14 bold">{{ item.krName }}</div>
           </div>
-          <div class="mostitem_count" style="color: #ff9900">
-            {{ item.championUsedCount }}
-          </div>
-          <div class="mostitem_desc" v-html="item.desc"></div>
-
+          <el-popover
+              class="p-relative w100p h100p"
+              style="top: 10px;"
+              placement="bottom"
+              :width="'100%'"
+              trigger="manual"
+              :title="item.krName"
+              v-model="visible">
+              <div v-html="item.desc"></div>
+          </el-popover>
+<!--          <div class="mostitem_desc" v-html="item.desc"></div>-->
       </div>
   </div>
 </template>
@@ -17,9 +26,14 @@
 export default {
     name: "MostItem",
     props: ['mostItem'],
+    data() {
+        return {
+            visible: true,
+        }
+    }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
